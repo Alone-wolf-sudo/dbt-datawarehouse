9a8c378a8195e3c0ca67e9e7df4220ddd6376230
chandy raccha <17n81a0586.chandu@gmail.com>
2023-10-05 13:02:34 +0000

    created sources and only one model

--- models/temp_models/test_model.sql
+++
@@ -1,5 +0,0 @@
-
--- Use the `ref` function to select from other models
-
-select date
-from {{ ref('stg_covid_19') }}
--- tests/assert_stg_covid_19_new_confirmed.sql
+++
@@ -1,5 +0,0 @@
-with confirmed_cases AS (
-    SELECT * FROM {{ ref('stg_covid_19') }}
-)
-
-SELECT country_code,count(new_confirmed)  AS cases FROM confirmed_cases GROUP BY country_code HAVING cases<0 
